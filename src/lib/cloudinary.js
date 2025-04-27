8c4d16eb9517dce5f41a3665ea0fe8495ee1352c
rlaehdwn0105 <140576604+rlaehdwn0105@users.noreply.github.com>
2025-04-27 13:52:34 +0900

    Delete src/lib directory

--- src/lib/cloudinary.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from "cloudinary";
-
-cloudinary.config({
-    cloud_name: dj9u6tpsv,
-    api_key: 156324736468568,
-    api_secret: MFjnc0-GKoQ6Asv4jLOoV-Sabuc,
-});
-
-export default cloudinary;
