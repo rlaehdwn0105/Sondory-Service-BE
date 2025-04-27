85562823545eec0d60bfa12deba66d5ba6e32eb1
rlaehdwn0105 <140576604+rlaehdwn0105@users.noreply.github.com>
2025-04-27 13:50:53 +0900

    Update song.js

--- src/models/song.js
+++ src/models/song.js
@@ -1,52 +1,56 @@
 import Sequelize from 'sequelize';
 
 class Song extends Sequelize.Model {
-	  static initModel(sequelize) {
-		      return super.init({
-			            title: {
-					            type: Sequelize.STRING(100),
-					            allowNull: false,
-					            unique: true,
-					          },
-			            coverUrl: {
-					            type: Sequelize.STRING,
-					            allowNull: true,
-					          },
-			            audioUrl: {
-					            type: Sequelize.STRING,
-					            allowNull: false,
-					          },
-			            duration: {
-					            type: Sequelize.INTEGER,
-					            allowNull: false,
-					          },
-			          }, {
-					        sequelize,
-					        modelName: 'Song',
-					        tableName: 'songs',
-					        timestamps: true,
-					        collate: 'utf8_general_ci',
-					        charset: 'utf8',
-					      });
-		    }
+  static initModel(sequelize) {
+    return super.init(
+      {
+        title: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+          unique: true,
+        },
+        coverUrl: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        audioUrl: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        duration: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: 'Song',
+        tableName: 'songs',
+        timestamps: true,
+        collate: 'utf8_general_ci',
+        charset: 'utf8',
+      }
+    );
+  }
 
-	  static associate(db) {
-		      this.belongsTo(db.User, { foreignKey: 'UploaderId' });
+  static associate(db) {
+    this.belongsTo(db.User, {
+      foreignKey: 'UploaderId',
+    });
 
-		      this.belongsToMany(db.User, {
-			            through: db.RecentPlay,
-			            foreignKey: 'SongId',
-			            otherKey: 'UserId',
-			            as: 'RecentListeners',
-			          });
+    this.belongsToMany(db.User, {
+      through: db.RecentPlay,
+      foreignKey: 'SongId',
+      otherKey: 'UserId',
+      as: 'RecentListeners',
+    });
 
-		      this.belongsToMany(db.User, {
-			            through: 'likes',
-			            foreignKey: 'SongId',
-			            as: 'Likers',
-			          });
-		    }
+    this.belongsToMany(db.User, {
+      through: 'likes',
+      foreignKey: 'SongId',
+      as: 'Likers',
+    });
+  }
 }
 
 export default Song;
-
