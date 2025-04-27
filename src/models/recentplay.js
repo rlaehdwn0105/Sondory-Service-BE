a38d294b3987bf822bfa76fbd2175928b8576cbb
rlaehdwn0105 <140576604+rlaehdwn0105@users.noreply.github.com>
2025-04-27 13:50:29 +0900

    Update recentplay.js

--- src/models/recentplay.js
+++ src/models/recentplay.js
@@ -1,46 +1,45 @@
 import Sequelize from 'sequelize';
 
 class RecentPlay extends Sequelize.Model {
-	  static initModel(sequelize) {
-		      return super.init(
-			            {
-					            playedAt: {
-							              type: Sequelize.DATE,
-							              allowNull: false,
-							              defaultValue: Sequelize.NOW,
-							            },
-					            UserId: {
-							              type: Sequelize.INTEGER,
-							              allowNull: false,
-							            },
-					            SongId: {
-							              type: Sequelize.INTEGER,
-							              allowNull: false,
-							            },
-					          },
-			            {
-					            sequelize,
-					            modelName: 'RecentPlay',
-					            tableName: 'recent_plays',
-					            charset: 'utf8',
-					            collate: 'utf8_general_ci',
-					            timestamps: true,
-					          }
-			          );
-		    }
+  static initModel(sequelize) {
+    return super.init(
+      {
+        playedAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.NOW,
+        },
+        UserId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        SongId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: 'RecentPlay',
+        tableName: 'recent_plays',
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+        timestamps: true,
+      }
+    );
+  }
 
-	  static associate(db) {
-		      this.belongsTo(db.User, {
-			            foreignKey: 'UserId',
-			            onDelete: 'CASCADE',
-			          });
+  static associate(db) {
+    this.belongsTo(db.User, {
+      foreignKey: 'UserId',
+      onDelete: 'CASCADE',
+    });
 
-		      this.belongsTo(db.Song, {
-			            foreignKey: 'SongId',
-			            onDelete: 'CASCADE',
-			          });
-		    }
+    this.belongsTo(db.Song, {
+      foreignKey: 'SongId',
+      onDelete: 'CASCADE',
+    });
+  }
 }
 
 export default RecentPlay;
-
