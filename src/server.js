d6c59b02b3c0f30f57131777ff9dff26dd5a6f90
rlaehdwn0105 <rlaehdwn0105@gmail.com>
2025-05-28 02:30:23 +0900

    update

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ import './tracing.js';
 import app from './app.js';
 
 app.listen(app.get('port'), '0.0.0.0', () => {
-  console.log(`Server is running on port ${app.get('port')}`);
+  console.log(`Server is running on port! ${app.get('port')}`);
 });
 
